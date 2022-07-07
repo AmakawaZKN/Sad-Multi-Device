9a6a6c3099438287c33bb3f798b38b4e803deb34
AmakawaZKN <108383788+AmakawaZKN@users.noreply.github.com>
2022-07-07 14:47:24 +0700

    Update info-bioowner.js

--- plugins/info-bioowner.js
+++ plugins/info-bioowner.js
@@ -1,24 +1,23 @@
 let handler = async (m, { conn }) => {
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `*───────[ BIODATA OWNER ]───────*
-*💌 Nama* : Sad-Bot
-*✉️ Nama RL* : Fahri
+*💌 Nama* : Jiikuan
+*✉️ Nama RL* : Zykuan
 *♂️ Gender* : Laki - laki
 *🕋 Agama* : Islam
-*⏰ Tanggal lahir* : 10 Juni 2007
+*⏰ Tanggal lahir* : 03 Desember 2007
 *🎨 Umur* : 15
-*🧮 Kelas* : 9
-*🧩 Hobby* : Nonton Hanime, Chatting, Recode script bot
-*💬 Sifat* : Idiot, Tidak Ramah, Kanjut, Prik, Pedofil
-*🗺️ Tinggal* : Indo, Riau, Indragiri Hulu, Air Molek
-*❤️ Suka* : warnah kuning & biru, Hanime, waifu, loli, trap, furry, kucing
-*💔 Benci* : autis, anak epep, seleb
+*🧩 Hobby* : Nonton Animek, Game, Tiktokan,
+*💬 Sifat* : Ramah, Baik, Pendiam
+*🗺️ Tinggal* : Japan, Tokyo, Akihabara
+*❤️ Suka* : Shikimori, Shikimori, Kamu (bukan laki ya anj), Kamu (Tapi ga peka)
+*💔 Benci* : autis, seleb, pedofil, hina waifu gw🤠🥀
 
 *───────[ SOSIAL MEDIA ]───────*
-*📷 instagran* : @bot_whangsaf
-*🇫  Facebook* : Adison Fahri
-*🏮 Chanel Youtube* : Papah-Chan
-*🐈 Github:* FahriAdison
+*📷 instagran* : -
+*🇫  Facebook* : -
+*🏮 Chanel Youtube* : -
+*🐈 Github:* AmakawaZKN
 
 `
 conn.sendHydrated(m.chat, teksbio, wm, ppown, "wa.me/" + nomorown, "💬 ᴄʜᴀᴛs", null,null, [["ᴅᴏɴᴀsɪ", '.donasi'], [null, null],[null,null]], m)
